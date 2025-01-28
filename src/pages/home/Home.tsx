e07a82ae1ed83a62d1e1388545490f86c56cc1eb
Martinelii <Kevenmartineli@outlook.com>
2025-01-28 14:04:20 -0300

    style: Ajustando Margin e Texto componente Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,16 +9,16 @@ function Home() {
                     <h2 className="text-5xl font-bold">
                         RHsys
                     </h2>
-                    <p className="text-xl max-w-4xl text-center">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum elit elit, sed accumsan augue sed accumsan augue.
+                    <p className="text-xl max-w-4xl text-center mt-7">
+                        Um sistema de gerenciamento para Recursos Humanos, que busca facilitar o processo de cadastro e acompanhamento dos funcionários, gerenciando as informações em um único lugar e fornecendo relatórios personalizados.
                     </p>
 
                     <div className="flex flex-row">
-                    <CardHome titulo="Lorem ipsum" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    <CardHome titulo="Funcionários" descricao="Foco no gerenciamento dos dados e cargos dos funcionários"
                                     img="https://i.imgur.com/5ySMOvG.png" alt="Person icon by Icons8"/>
-                    <CardHome titulo="Lorem ipsum" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    <CardHome titulo="Contratos" descricao="Facilidade de inserir e gerenciar os contratos de trabalho."
                                     img="https://i.imgur.com/ZntN5og.png" alt="Pen icon by Icons8"/>
-                    <CardHome titulo="Lorem ipsum" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    <CardHome titulo="Ferramentas" descricao="Facilidade nas análises através da ferramente de relatórios personalizados."
                                     img="https://i.imgur.com/jTT4MEv.png" alt="Gear icon by Icons8"/>
                     </div>
                     
