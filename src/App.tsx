e574468fa8502c2e7275accc244fd8eafc67e029
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2025-01-28 13:16:04 -0300

    feat: criacao do componente sobre nos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import SobreNos from './components/sobrenos/SobreNos'
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <div className='min-h-[88.7dvh]'>
-            
-          </div>
-        </Routes>
+        <div className='min-h-[88.7dvh]'>  
+          <Routes>
+            <Route path="/sobrenos" element={<SobreNos />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   )
